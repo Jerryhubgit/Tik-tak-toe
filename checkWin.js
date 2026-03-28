ed614e4018346f63205463a0b73cf5dceaa14776
jerryhubgit <chimobijeremaih@gmail.com>
2026-03-28 16:53:25 +0100

    terminal version of the game completed 90%

--- checkWin.js
+++ checkWin.js
@@ -3,47 +3,54 @@ const WINNING_MOVES  = [[1,2,3], [4,5,6], [7,8,9],
                         [1,5,9], [3,5,7]]
 
 
+
+
+const moves = [1,0,9,5]
+
 // loop through array 
-// selected a row
-// loop select element in moves
-// check if element is in row
-    // if true -> count++
-        // check if all of them are there 
-            // count === 3 
-                // moves ***** is at position ***
-    // else - break out of loop
+// select a row
+// select an item from the moves
+// check if the move is the winning list
+    // if its in the winnig list
+        // count++
+
+            // if count === 3 
+                // print the move 
+                // break;
+    // else continue
 
 
-
-const moves = [3,7,5] 
-
-export default function CheckWin(moves){
-    let searchCount = -1;
+    
+export default function checkWin(moves){
+    let searchCount = 0
+    let playerWon = false;
     for(let rows in WINNING_MOVES){
-        let count = 0
-        let selectedRow = [...WINNING_MOVES[rows]]
-        let isInTheWinningList;
-        searchCount++
-        for(let column in moves){
-            isInTheWinningList = selectedRow.includes(moves[column])
-            console.log(isInTheWinningList, selectedRow, moves[column])
-            if(isInTheWinningList){
-                count++
-                if(count === 3) { 
-                    console.log(`\x1b[32mMoves ${moves} at index ${searchCount} is a winning move\x1b[0m`)
-                    break;
-                }
-                selectedRow.splice(selectedRow.indexOf(moves[column]), 1)
-            }else {
-                console.log(`\x1b[31mMoves ${moves} isn't a winning move\x1b[0m`)
-                break;
-            }
-        }
-        if(count === 3) break;
-    }
-}
+    let count = 0;
+    let selectedRow = WINNING_MOVES[rows]
+        for(let columns in moves){
+            searchCount++;
+            let isInWinningList = selectedRow.includes(moves[columns])
 
-// CheckWin(moves)
+            if(isInWinningList){
+                count++
+
+                if(count === 3){
+                    playerWon = true
+                    return playerWon
+                }
+            }else {
+                // console.log('\x1b[31m not in winning list \x1b[0m')
+                continue; 
+            } 
+        }
+
+        if(count === 3) break;
+        if(searchCount === 32) return playerWon
+    }
+    
+
+
+}
 
 
 
--- script.js
+++ script.js
@@ -23,16 +23,15 @@
 // resetGame
 }
 
-// green color
+
 import checkWin from './checkWin.js'
 import readline from  'readline'
-
+                                                  
 const GAME_BOARD  = [[1, 2, 3], 
                      [4, 5, 6], 
                      [7, 8, 9]]
-const WINNING_MOVES  = [[1,2,3], [4,5,6], [7,8,9], 
-                        [1,4,7], [2,5,8], [3,6,9], 
-                        [1,5,9], [3,5,7]]
+
+const availableSpots = [1,2,3,4,5,6,7,8,9]
 
 let currentMove = 'X'
 const player_X_moves = []
@@ -40,7 +39,7 @@ const player_O_moves = []
 let [player_X_won, player_O_won] = [false, false]
 
 
-function showGameBoard(){
+const showGameBoard = () => {
     for(let rows in GAME_BOARD){
         console.log(GAME_BOARD[rows])
     }
@@ -57,41 +56,83 @@ const play = (position) => {
     }
 }
 
+const updateMove = () => {
+    currentMove = currentMove === "X" ? 'O' : 'X'
+}
+
 const readInput = readline.createInterface({
     input: process.stdin, 
     output: process.stdout
 })
 
-let totalMoves = 0
+
+
+const positionIsInvalid = (position) => {
+   return ( isNaN(position) || position < 1 || position > 9 )
+}
+
+let totalMoves =  9 - availableSpots.length;
+showGameBoard()
 function playGame(){
+    console.log(`AVAILABLE SPOT: [${availableSpots}]`)
+
     // accept input
-    readInput.question(`choose and available spot and Play ( X ): `, (position) => {
+    readInput.question(`choose and available spot and Play ( ${currentMove} ): `, (position) => {
         position = Number(position)
-        if(isNaN(position) || position < 1 || position > 9){
+
+        if(positionIsInvalid(position)){
             console.log('\x1b[31m input is invalid\x1b[0m')
             console.log('Choose from the available spot on the game board')
             showGameBoard()
             playGame()
-        }else{
+        }
+        else if (!availableSpots.includes(position)){
+            console.log(`\x1b[31m That spot has already been taken\x1b[0m`)
+            showGameBoard()
+            playGame()
+        }
+        else{
+            // position is available 
+                // if true - do
+                // else
             play(position)
+            console.log(position)
+            availableSpots.splice(availableSpots.indexOf(position), 1)
 
             if(currentMove === 'X') player_X_moves.push(position)
             else player_O_moves.push(position)
+            console.log(`X: ${player_X_moves} `, `O: ${player_O_moves}`)
     
-            totalMoves += 1
+            // totalMoves += 1
             console.log(`\x1b[32m Move Executed at position ${position} \x1b[0m `)
 
             // check if the player is likely to start winning
-            if(totalMoves < 5){
+            if(totalMoves > 5){
                 showGameBoard()
+                updateMove()
+                playGame()
             }else{
                 // check won 
                     // x won 
-                    checkWin(player_X_moves)
-                    checkWin(player_O_moves)
+                    if(checkWin(player_X_moves)) {
+                        console.log(`\x1b[32m Player x : ${checkWin(player_X_moves) == true ? 'won' : 'lost'} \x1b[0m`)
+                        readInput.close()
+                    }else if (checkWin(player_O_moves)){
+                        console.log(`]\x1b[32m Player O : ${checkWin(player_O_moves) == true ? 'won' : 'lost'} \x1b[0m`)
+                        readInput.close()
+                    }else{
+                        if(availableSpots.length === 0) {
+                            console.log(`\x1b[33m Argh!!, the game was draw\x1b[0m`)
+                            readInput.close()
+                        }
+                        else {
+                            console.log('stil in game');
+                            showGameBoard()
+                            updateMove()
+                            playGame()
+                        }
+                    }
                     // if draw -> break
-                showGameBoard()
-                readInput.close()
             }
             
         }
@@ -101,3 +142,7 @@ function playGame(){
 
 
 playGame()
+
+
+// confirm  X is a winning move
+// X: 1,3,9,6  O: 5,2,4
